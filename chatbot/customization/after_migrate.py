b08b5bb6abd680843da01941cb8cd1ce5a62b875
DHINESH00 <18csa09@karpagamtech.ac.in>
2024-09-26 12:12:03 +0530

    ref:add custum feilds  when selecting Party Type

--- chatbot/customization/after_migrate.py
+++ chatbot/customization/after_migrate.py
@@ -1,8 +1,4 @@
-from chatbot.customization.customer.custom_field import customer_field_customization
-from chatbot.customization.supplier.custom_field import supplier_field_customization
-from chatbot.customization.employee.custom_field import employee_field_customization
+
 
 def after_migrate():
-    customer_field_customization()
-    supplier_field_customization()
-    employee_field_customization()
+    pass
