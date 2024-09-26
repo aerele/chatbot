3ba00cb80108d38c1ea12d8c04ef920bde896ef5
vimalraj27 <vimalraj2772000@gmail.com>
2024-09-26 15:46:53 +0530

    Some changes in chatbot doctypes

--- chatbot/chatbot/doctype/chatbot_message_template/chatbot_message_template.js
+++ chatbot/chatbot/doctype/chatbot_message_template/chatbot_message_template.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Aerele and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Chatbot Message Template", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Chatbot Message Template", {
+	refresh(frm) {
+        frm.set_query("print_format", function (doc) {
+			return {
+				filters: {
+					"doc_type": frm.doc.default_doctype
+				},
+			};
+		});
+	},
+});
