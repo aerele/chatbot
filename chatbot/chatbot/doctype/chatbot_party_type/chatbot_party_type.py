529ec5586fb24829f1854f90e4033ababf9bf0c3
DHINESH00 <18csa09@karpagamtech.ac.in>
2024-09-26 13:44:30 +0530

    ref:add custom feilds

--- chatbot/chatbot/doctype/chatbot_party_type/chatbot_party_type.py
+++ chatbot/chatbot/doctype/chatbot_party_type/chatbot_party_type.py
@@ -12,28 +12,39 @@ class ChatbotPartyType(Document):
 
 def create_customer_custom_field(party_name):
 	enable_telegram,enable_whatsapp,enable_slack=frappe.db.get_value("Chatbot Setup","Chatbot Setup",["enable_telegram","enable_whatsapp","enable_slack"])
+	insert_afrter_fieldname= frappe.get_meta(party_name)
+	insert_afrter_fieldname=insert_afrter_fieldname.fields[-1].get("fieldname")
+	print(insert_afrter_fieldname)
 	custom_fields = {}
 	if enable_telegram:
-		custom_fields.update( {
+		custom_fields = {
 			party_name: [
 				dict(
 					fieldname="chatbot_details",
 					label="Chatbot Details",
 					fieldtype="Tab Break",
+					insert_afrter=insert_afrter_fieldname,
 				),
 				dict(
-					fieldname="custom_telegram_username",
+					fieldname="sb_chb",
+					label="",
+					fieldtype="Section Break",
+					insert_afrter="chatbot_details"
+				),
+				dict(
+					fieldname="telegram_username",
 					label="Telegram Username",
 					fieldtype="Data",
-					insert_after="chatbot_details",
+					insert_after="sb_chb",
 				),
 				dict(
 					fieldname="telegram_user_id",
 					read_only=1,
 					label="",
 					fieldtype="Data",
-					insert_after="custom_telegram_username",
+					insert_after="telegram_username",
 				),
 			]
-		})
+		}
+		print(custom_fields)
 	create_custom_fields(custom_fields, update=True)
